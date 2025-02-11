08f834bad2ff6b577ad47adb8050ba1e8025e692
Kailie Peterson <kailie.peterson04@gmail.com>
2025-02-11 04:09:32 -0500

    Update feed and home files to include daily prompt section in feed

--- app/index.jsx
+++ app/index.jsx
@@ -1,48 +1,42 @@
 import React from 'react';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-<<<<<<< HEAD
-
-=======
->>>>>>> 830deb6761211d16e1b2cda95360deb33c87b0d2
 import Welcome from './welcome';
+import Signup from './signup';
 import Home from './home';
 import Settings from './settings';
-import Signup from './signup';
 
 const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    
-      <Stack.Navigator
-        initialRouteName="Welcome"
-        screenOptions={{
-          headerStyle: { backgroundColor: '#1E90FF' },
-          headerTintColor: '#fff',
-          headerTitleStyle: { fontWeight: 'bold' },
-        }}
-      >
-        <Stack.Screen
-          name="Welcome"
-          component={Welcome}
-          options={{ headerShown: false }}
-        />
-        <Stack.Screen
-         name = "Signup"
-         component={Signup}
-         options = {{headerShown: false}}
-        />
-        <Stack.Screen
-          name="Home"
-          component={Home}
-          options={{ headerShown: false }}
-        />
-        <Stack.Screen
-          name="Settings"
-          component={Settings}
-          options={{ title: 'Settings' }}
-        />
-      </Stack.Navigator>
-   
+    <Stack.Navigator
+      initialRouteName="Welcome"
+      screenOptions={{
+        headerStyle: { backgroundColor: '#1E90FF' },
+        headerTintColor: '#fff',
+        headerTitleStyle: { fontWeight: 'bold' },
+      }}
+    >
+      <Stack.Screen
+        name="Welcome"
+        component={Welcome}
+        options={{ headerShown: false }}
+      />
+      <Stack.Screen
+        name="Signup"
+        component={Signup}
+        options={{ headerShown: false }}
+      />
+      <Stack.Screen
+        name="Home"
+        component={Home}
+        options={{ headerShown: false }}
+      />
+      <Stack.Screen
+        name="Settings"
+        component={Settings}
+        options={{ title: 'Settings' }}
+      />
+    </Stack.Navigator>
   );
 }
