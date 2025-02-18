2433fc5abf3503a328ae5ddc7e357fff0a24a4a8
Krutika Patil <krutikapatil125@gmail.com>
2025-02-18 11:01:11 -0500

    Added username field and connected to firestore

--- app/signup.jsx
+++ app/signup.jsx
@@ -4,11 +4,14 @@ import {View, TextInput, Button, Text, StyleSheet} from 'react-native';
 import auth from "@react-native-firebase/auth";
 import db from "@react-native-firebase/database";
 import { useNavigation } from 'expo-router';
+import firestore from '@react-native-firebase/firestore';
+//import { getFirestore } from '@react-native-firebase/firestore';
 
 export default function signup(){
     const [email,setEmail] = useState('');
     const [password,setPassword] = useState('');
     const [message,setMessage] = useState('');
+    const [username, setUsername] = useState('');
     const navigation = useNavigation()
 
     const handlesignup = () => {
@@ -16,11 +19,33 @@ export default function signup(){
         .createUserWithEmailAndPassword(email,password)
         .then(userCredentials => {
           const user = userCredentials.user;
-          console.log(user.email);
+          console.log("USer created:",user.email, "UID", user.uid);
+
+          //store user infor in firestore
+          firestore()
+            .collection('users')
+            .doc(user.uid)
+            .set({
+              username: username,
+              email: user.email,
+            });
+          /*
+          const firestore = getFirestore();
+          const userDocRef = doc(firestore,'users',user.uid)
+          return setDoc(userDocRef, {
+            username:username,
+            email:user.email,
+          });
+          */
+        })
+        .then(()=>{
+          console.log("User successfully added to firestore");
           setMessage('Signup Successful!');
         })
-        .catch(error => alert(error.message))
-        
+        .catch(error=>{
+          console.error("Firestore error:", error);
+          alert(error.message);
+        });
     };
 
     const handlelogin = () => {
@@ -38,6 +63,13 @@ export default function signup(){
 
     return (
         <View style={styles.container}>
+          <TextInput
+            style={styles.input}
+            placeholder="Username"
+            value={username}
+            onChangeText={setUsername}
+            autoCapitalize='none'
+          />
           <TextInput
             style={styles.input}
             placeholder="Email"
