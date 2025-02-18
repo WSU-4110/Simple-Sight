5a57b5f7b64702d495c28600d97f07c5ef563668
jacobc046 <jacob.croket@wayne.edu>
2025-02-18 14:17:13 -0500

    expo camera working

--- app/camera.tsx
+++ app/camera.tsx
@@ -1,19 +1,23 @@
-import { useState } from "react";
-import { CameraView, CameraType, useCameraPermissions } from 'expo-camera';
-import { Button, StyleSheet, View, Text } from "react-native";
+import { useRef, useState } from "react";
+import { CameraView, CameraType, useCameraPermissions, FlashMode } from 'expo-camera';
+import { Button, StyleSheet, View, Text, TouchableOpacity } from "react-native";
+import Ionicons from 'react-native-vector-icons/Ionicons';
+
 
 export default function Camera() {
     const [permission, requestPermission] = useCameraPermissions();
     const [facing, setFacing] = useState<CameraType>('back');
+    const [flashMode, setFlashMode] = useState<FlashMode>('off');
+    const ref = useRef<CameraView>(null);
 
     if (!permission) {
-        return(
+        return (
             <View>
                 <Text>Loading...</Text>
             </View>
         );
     } else if (!permission.granted) {
-        return(
+        return (
             <View>
                 <Text>Please enable camera permissions to get the best experience using Simple Sight</Text>
                 <Button title="Enable camera" onPress={requestPermission} />
@@ -25,9 +29,30 @@ export default function Camera() {
         setFacing(current => (current === 'back' ? 'front' : 'back'));
       }
 
+    function toggleFlashMode() {
+        const modes: Array<FlashMode> = ['off', 'auto', 'on'];
+        var currentMode = modes.indexOf(flashMode);
+
+        setFlashMode(modes[++currentMode % 3]);
+    }
+
+    async function takePhoto() {
+        const photo = await ref.current?.takePictureAsync();
+    }
+
+    const size: number = 24;
+
     return (
         <CameraView facing={facing}>
-            
+            <TouchableOpacity id="cameraReverse" className="cameraControl">
+                <Ionicons name="camera-reverse-outline" size={size} />
+            </TouchableOpacity>        
+            <TouchableOpacity id="flash" className="cameraControl" onPress={toggleFlashMode}>
+                <Ionicons name={flashMode == 'off' ? 'flash-off-outline' : 'flash-outline'} size={size} /> 
+            </TouchableOpacity>
+            <TouchableOpacity id="shutter" className="cameraControl" onPress={takePhoto}>
+                <Ionicons name="ellipse-outline" size={48} /> 
+            </TouchableOpacity>
         </CameraView>
     );
 }
@@ -35,5 +60,8 @@ export default function Camera() {
 const styles = StyleSheet.create({
     camera: {
 
+    },
+    cameraControl: {
+        
     },
 })
