509bb6df14410638be924b5d7b11717f78e8cb82
jacobc046 <jacob.croket@wayne.edu>
2025-02-23 12:20:36 -0500

    Added basic camera features and funcitons

--- app/camera.tsx
+++ app/camera.tsx
@@ -1,67 +1,146 @@
-import { useRef, useState } from "react";
-import { CameraView, CameraType, useCameraPermissions, FlashMode } from 'expo-camera';
-import { Button, StyleSheet, View, Text, TouchableOpacity } from "react-native";
-import Ionicons from 'react-native-vector-icons/Ionicons';
+import { CameraMode, CameraType, CameraView, FlashMode, useCameraPermissions } from "expo-camera";
+  import { useRef, useState } from "react";
+  import { Button, Pressable, StyleSheet, Text, TouchableOpacity, View } from "react-native";
+  import { Image } from "expo-image";
+  import Ionicons from 'react-native-vector-icons/Ionicons';
+import { name } from '../node_modules/ci-info/index.d';
 
-
-export default function Camera() {
+  export default function App() {
     const [permission, requestPermission] = useCameraPermissions();
-    const [facing, setFacing] = useState<CameraType>('back');
-    const [flashMode, setFlashMode] = useState<FlashMode>('off');
     const ref = useRef<CameraView>(null);
-
+    const [uri, setUri] = useState<string | undefined>(undefined);
+    const [facing, setFacing] = useState<CameraType>("back");
+    const [flashMode, setFlashMode] = useState<FlashMode>('off');
+    const iconSize: number = 32;
+  
     if (!permission) {
-        return (
-            <View>
-                <Text>Loading...</Text>
-            </View>
-        );
-    } else if (!permission.granted) {
-        return (
-            <View>
-                <Text>Please enable camera permissions to get the best experience using Simple Sight</Text>
-                <Button title="Enable camera" onPress={requestPermission} />
-            </View>
-        );
+      return null;
     }
+  
+    if (!permission.granted) {
+      return (
+        <View style={styles.container}>
+          <Text style={{ textAlign: "center" }}>
+            We need your permission to use the camera
+          </Text>
+          <Button onPress={requestPermission} title="Grant permission" />
+        </View>
+      );
+    }
+  
+    const takePicture = async () => {
+      const photo = await ref.current?.takePictureAsync();
+      setUri(photo?.uri);
+    };
+  
+    const toggleFacing = () => {
+      setFacing((prev) => (prev === "back" ? "front" : "back"));
+    };
 
-    function toggleCameraFacing() {
-        setFacing(current => (current === 'back' ? 'front' : 'back'));
-      }
-
-    function toggleFlashMode() {
+    const toggleFlashMode = () => {
         const modes: Array<FlashMode> = ['off', 'auto', 'on'];
         var currentMode = modes.indexOf(flashMode);
+        const newMode = modes[++currentMode % 3]
 
-        setFlashMode(modes[++currentMode % 3]);
+        setFlashMode(newMode);
     }
 
-    async function takePhoto() {
-        const photo = await ref.current?.takePictureAsync();
+    async function uploadPhoto() {
+        //upload photo
     }
-
-    const size: number = 24;
-
-    return (
-        <CameraView facing={facing}>
-            <TouchableOpacity id="cameraReverse" className="cameraControl">
-                <Ionicons name="camera-reverse-outline" size={size} />
-            </TouchableOpacity>        
-            <TouchableOpacity id="flash" className="cameraControl" onPress={toggleFlashMode}>
-                <Ionicons name={flashMode == 'off' ? 'flash-off-outline' : 'flash-outline'} size={size} /> 
+  
+    const renderPicture = () => {
+      return (
+        <View>
+          <Image
+            source={{ uri }}
+            contentFit="contain"
+            style={{ flex: 1, aspectRatio: 1 }}
+          />
+          <View id="previewOptions">
+            <TouchableOpacity onPress={() => setUri(undefined)} style={{ backgroundColor: 'red'}}>
+                <Text>Delete</Text>
             </TouchableOpacity>
-            <TouchableOpacity id="shutter" className="cameraControl" onPress={takePhoto}>
-                <Ionicons name="ellipse-outline" size={48} /> 
+            <TouchableOpacity onPress={uploadPhoto} style={{ backgroundColor: 'green'}}>
+                <Text>Save</Text>
             </TouchableOpacity>
+          </View>
+        </View>
+      );
+    };
+  
+    const renderCamera = () => {
+      return (
+        <CameraView
+          style={styles.camera}
+          ref={ref}
+          mode={'picture'}
+          facing={facing}
+          mute={false}
+          responsiveOrientationWhenOrientationLocked
+          flash={flashMode}
+        >
+          <View style={styles.shutterContainer}>
+            <Pressable onPress={takePicture}>
+                <Ionicons name="ellipse-outline" size={iconSize*3}/>
+            </Pressable>
+            <Pressable onPress={toggleFacing}>
+                <Ionicons name="camera-reverse-outline" size={iconSize} />
+            </Pressable>
+            <Pressable onPress={toggleFlashMode}>
+                <Ionicons name={flashMode == 'off' ? 'flash-off-outline' : 'flash-outline'} size={iconSize}/> 
+            </Pressable>
+          </View>
         </CameraView>
+      );
+    };
+  
+    return (
+      <View style={styles.container}>
+        {uri ? renderPicture() : renderCamera()}
+      </View>
     );
-}
-
-const styles = StyleSheet.create({
+  }
+  
+  const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: "#fff",
+      alignItems: "center",
+      justifyContent: "center",
+    },
     camera: {
-
+      flex: 1,
+      width: "100%",
     },
-    cameraControl: {
-        
+    shutterContainer: {
+      position: "absolute",
+      bottom: 44,
+      left: 0,
+      width: "100%",
+      alignItems: "center",
+      flexDirection: "row",
+      justifyContent: "space-between",
+      paddingHorizontal: 30,
     },
-})
+    shutterBtn: {
+      backgroundColor: "transparent",
+      borderWidth: 5,
+      borderColor: "white",
+      width: 85,
+      height: 85,
+      borderRadius: 45,
+      alignItems: "center",
+      justifyContent: "center",
+    },
+    shutterBtnInner: {
+      width: 70,
+      height: 70,
+      borderRadius: 50,
+    },
+    previewOptions: {
+        flex: 1,
+        flexDirection: 'row',
+        alignItems: 'center',
+    }
+  });
