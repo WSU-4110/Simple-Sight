d2445804daa770e2a6d4e1e9e3cfaf6d42242752
Kailie Peterson <kailie.peterson04@gmail.com>
2025-03-03 12:50:43 -0500

    Resolved merge conflicts with main

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -3,20 +3,10 @@ import { Stack } from 'expo-router'
 
 export default function _layout() {
   return (
-    //this must be in _layout to ignore safe areas are filled
     <Stack 
-    screenOptions={{
+      screenOptions={{
         headerShown: false
-    }}
+      }}
     />
   )
 }
-
-//THIS CAN ALSO BE USED ON INDIVIDUAL COMPONENTS not nav ones
-// const insets = useSafeAreaInsets();
-// <View
-//       style={{
-//         marginBottom: -insets.bottom,
-//         marginTop: -insets.top,
-//       }}
-//     />
